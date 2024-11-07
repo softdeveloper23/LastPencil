aa869bb870f7a5547ae2a410041378a8e306cae8
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-07 12:05:39 +0100

    Added new rules and code for the game

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,9 +1,19 @@
 package lastpencil;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("|||||||");
-        System.out.println("Your turn!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pencils would you like to use:");
+        int pencils = scanner.nextInt();
+        System.out.println("Who will be the first (John, Jack):");
+        String firstName = scanner.next();
+        for (int i = 1; i <= pencils; i++) {
+            System.out.print("|");
+        }
+        System.out.println();
+        System.out.println(firstName + " is going first!");
 
     }
 }
