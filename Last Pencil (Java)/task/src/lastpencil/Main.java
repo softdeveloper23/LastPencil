633fec57521a39eaa34bc232c85d72cb8ae507b8
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-07 12:47:12 +0100

    Updated code

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -8,17 +8,12 @@ public class Main {
         int pencils = 0;
         String john = "John";
         String jack = "Jack";
+        String firstPlayer = "";
+        String secondPlayer = "";
 
 
-        pencilAmount(scanner, pencils);
+        int gamePencils = pencilAmount(scanner, pencils);
 
-        System.out.println("Who will be the first (John, Jack):");
-        String firstName = scanner.next();
-        for (int i = 1; i <= pencils; i++) {
-            System.out.print("|");
-        }
-        System.out.println();
-        System.out.println(firstName + " is going first!");
 
     }
 
@@ -27,4 +22,27 @@ public class Main {
         pencils = scanner.nextInt();
         return pencils;
     }
+
+    private static void gameLoop(Scanner scanner, String john, String jack, int gamePencils,
+                                 String firstPlayer, String secondPlayer) {
+        System.out.println("Who will be the first (John, Jack):");
+        String input = scanner.next();
+
+        if (input.equals(john)) {
+            firstPlayer = "John";
+            secondPlayer = "Jack";
+        } else {
+            firstPlayer = "Jack";
+            secondPlayer = "John";
+        }
+
+        while (gamePencils > 0) {
+            for (int i = 1; i <= pencils; i++) {
+                System.out.print("|");
+            }
+            System.out.println();
+            System.out.println(firstName + " is going first!");
+        }
+    }
+
 }
