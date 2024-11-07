1e391283f118185f37d000db7aee53e9f1959a1f
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-07 11:53:07 +0100

    First commit

---
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -0,0 +1,9 @@
+package lastpencil;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|||||||");
+        System.out.println("Your turn!");
+
+    }
+}
